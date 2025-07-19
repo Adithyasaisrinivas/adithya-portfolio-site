4ebf1dd3160f507b1a434bc2baf6e54ef42a264c
Adithyasaisrinivas <141301593+Adithyasaisrinivas@users.noreply.github.com>
2025-07-20 00:32:39 +0530

    Update layout.tsx metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Adithya Sai Srinivas',
+  description: 'Portfolio Website',
+  generator: 'Adithya',
 }
 
 export default function RootLayout({
